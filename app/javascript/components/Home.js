afe128019e4cbaf6b55982fa6c0bb027678f5896
chucnm-1901 <nguyen.minh.chuc@sun-asterisk.com>
2019-10-14 10:21:08 +0700

    style header

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Header from './Header';
 class Home extends React.Component {
   render () {
     return (
       <React.Fragment>
+        <Header/>
         {this.props.name}
       </React.Fragment>
     );
